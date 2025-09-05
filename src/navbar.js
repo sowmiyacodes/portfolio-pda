5729f1ea95f6c4de95d9af88ebad55465af2dec6
sowmiyacodes <sowmiya9b@gmail.com>
2025-09-05 18:19:40 +0530

    navbar changes

--- src/navbar.js
+++ src/navbar.js
@@ -8,10 +8,10 @@ export default function navbar()
            <nav className="flex justify-between items-center px-8 py-4 bg-stone-800 text-white shadow-lg">
             <h1 className="text-xl font-bold">Sowmiya</h1>
             <ul className="flex space-x-6">
-                    <li><Link to="/" className="hover:text-yellow-400">About</Link></li>
-                    <li><Link to="/project" className="hover:text-yellow-400">Projects</Link></li>
-                    <li><Link to="/skill" className="hover:text-yellow-400">Skills</Link></li>
-                    <li><Link to="/contact" className="hover:text-yellow-400">Contact</Link></li>
+                    <li><Link to="/portfolio-pda" className="hover:text-yellow-400">About</Link></li>
+                    <li><Link to="/portfolio-pda/project" className="hover:text-yellow-400">Projects</Link></li>
+                    <li><Link to="/portfolio-pda/skill" className="hover:text-yellow-400">Skills</Link></li>
+                    <li><Link to="/portfolio-pda/contact" className="hover:text-yellow-400">Contact</Link></li>
             </ul>
             </nav>
         </div>
